fc81f694b274f782b1a5237a5f86c0fb3ddd7134
Hemanshu Khodiyar <hemanshu.eng@gmail.com>
2019-04-08 19:43:03 +0100

    products route

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -15,11 +15,15 @@ class ProductResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'name'=> $this->name,
-            'description'=> $this->detail,
+            'name' => $this->name,
+            'description' => $this->detail,
             'price' => $this->price,
-            'stock' => $this->stock,
-            'discount'=> $this->discount
+            'stock' => $this->stock == 0 ? 'Out of stock ' : $this->stock,
+            'discount' => $this->discount,
+            'totalPrice' => round((1 - ($this->discount / 100)) * $this->price, 2),
+            'rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('star') / $this->reviews->count(), 2) : 'No Rating yet',
+            'href' => ['reviews' => route('reviews.index', $this->id)]
+
         ];
     }
 }
